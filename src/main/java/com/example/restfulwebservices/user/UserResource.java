0bbbc39fbcee971a21b46e3f2dd5164d397fcd70
grhncnrbs <gurhan.cinarbas@hotmail.com>
2023-11-26 14:08:13 +0300

    .

--- src/main/java/com/example/restfulwebservices/user/UserResource.java
+++ src/main/java/com/example/restfulwebservices/user/UserResource.java
@@ -1,6 +1,7 @@
 package com.example.restfulwebservices.user;
 
 import com.example.restfulwebservices.exception.UserNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -31,7 +32,7 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User savedUser = userDaoService.save(user);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
         return ResponseEntity.created(location).build();
